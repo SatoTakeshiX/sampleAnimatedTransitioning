a3f540337a0bfe4347dcde979e4f9338a24ea750
satoutakeshi <sato.takeshi1987@gmail.com>
2015-08-15 20:30:32 +0900

    Initial Commit

---
+++ sampleAnimatedTransitioning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleAnimatedTransitioning
+//
+//  Created by satoutakeshi on 2015/08/15.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sampleAnimatedTransitioning/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sampleAnimatedTransitioning
+//
+//  Created by satoutakeshi on 2015/08/15.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
